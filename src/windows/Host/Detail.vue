1cdfdf8a29c91cbc2938b5a520bc3b86e5ecf856
BooguNote <boogunote@gmail.com>
2017-01-13 19:46:29 +0800

    draw chart

--- src/windows/Host/Detail.vue
+++ src/windows/Host/Detail.vue
@@ -1,10 +1,43 @@
 <script>
 import WindowBase from 'src/windows/Base/Window'
+import rpc from 'src/utils/rpc'
+import { hostCpuQueryExpression } from 'src/utils/utils'
 
 export default {
   mixins: [WindowBase],
   props: [
     'uuid'
-  ]
+  ],
+  data: function () {
+    return {
+      queryCpuDataTimerId: null
+    }
+  },
+  created: function () {
+    this.queryCpuDataTimerId = setInterval(() => {
+      this.queryCpuData()
+    }, 2000)
+  },
+  destroyed: function () {
+    this.clearTimer()
+  },
+  methods: {
+    queryCpuData: function () {
+      let self = this
+      rpc.call({
+        'org.zstack.prometheus.APIPrometheusQueryPassThroughMsg': {
+          expression: hostCpuQueryExpression('0', 'user', this.uuid),
+          relativeTime: '15m',
+          step: 9
+        }
+      }, (resp) => {
+        self.updateWindow({ cpuData: resp.inventories.data.result[0].values })
+        self.drawChart()
+      })
+    },
+    clearTimer: function () {
+      clearInterval(this.queryCpuDataTimerId)
+    }
+  }
 }
 </script>
--- src/windows/Host/DetailSidePage.vue
+++ src/windows/Host/DetailSidePage.vue
@@ -11,11 +11,13 @@
 
 <script>
 import * as d3 from 'd3'
+import _ from 'lodash'
 import Detail from './Detail'
 
 export default {
   mixins: [Detail],
   mounted: function () {
+    this.queryCpuData()
     this.drawChart()
   },
   updated: function () {
@@ -23,18 +25,20 @@ export default {
   },
   data () {
     return {
-      className: 'VmInstanceDetailSidePage'
+      className: 'HostDetailSidePage'
     }
   },
   methods: {
-    drawChart: () => {
+    drawChart: function () {
+      if (!this.windowData.cpuData) return
+      d3.select('.sidepage .content svg').remove()
       // set the dimensions and margins of the graph
       var margin = {top: 20, right: 20, bottom: 30, left: 50}
-      var width = 960 - margin.left - margin.right
+      var width = d3.select('.sidepage .content').node().getBoundingClientRect().width - margin.left - margin.right
       var height = 500 - margin.top - margin.bottom
 
       // parse the date / time
-      var parseTime = d3.timeParse('%d-%b-%y')
+      // var parseTime = d3.timeParse('%Y-%m-%d %H:%M:%S')
 
       // set the ranges
       var x = d3.scaleTime().range([0, width])
@@ -56,63 +60,38 @@ export default {
                 'translate(' + margin.left + ',' + margin.top + ')')
 
       // Get the data
-      d3.csv('http://127.0.0.1:9000/data.csv', function (error, data) {
-        if (error) throw error
-
-        // format the data
-        data.forEach(function (d) {
-          d.date = parseTime(d.date)
-          d.close = +d.close
-        })
-
-        // Scale the range of the data
-        x.domain(d3.extent(data, function (d) { return d.date }))
-        y.domain([0, d3.max(data, function (d) { return d.close })])
-
-        // Add the valueline path.
-        svg.append('path')
-            .data([data])
-            .style('fill', 'none')
-            .style('stroke', 'steelblue')
-            .style('stroke-width', '2px')
-            .attr('d', valueline)
-
-        // Add the X Axis
-        svg.append('g')
-            .attr('transform', 'translate(0,' + height + ')')
-            .call(d3.axisBottom(x))
-
-        // Add the Y Axis
-        svg.append('g')
-            .call(d3.axisLeft(y))
+      let data = _.cloneDeep(this.windowData.cpuData)
+      // format the data
+      data.forEach(function (d) {
+        d.date = new Date(d[0] * 1000)
+        d.close = parseFloat(d[1])
+        // d.close = +d.close
       })
+
+      // Scale the range of the data
+      x.domain(d3.extent(data, function (d) { return d.date }))
+      y.domain([0, d3.max(data, function (d) { return d.close })])
+
+      // Add the valueline path.
+      svg.append('path')
+          .data([data])
+          .style('fill', 'none')
+          .style('stroke', 'steelblue')
+          .style('stroke-width', '2px')
+          .attr('d', valueline)
+
+      // Add the X Axis
+      svg.append('g')
+          .attr('transform', 'translate(0,' + height + ')')
+          .call(d3.axisBottom(x))
+
+      // Add the Y Axis
+      svg.append('g')
+          .call(d3.axisLeft(y))
     }
   }
 }
 </script>
 
 <style scoped>
-.sidepage {
-  position: absolute;
-  top: 60px;
-  bottom: 0;
-  right: 0;
-  width: 50%;
-  height: calc(100% - 60px);
-  background-color: #fff;
-  overflow: hidden;
-  z-index: 10;
-  box-shadow: -5px 0 0 rgba(0,0,0,.07);
-}
-.header {
-  width: 100%;
-  height: 150px;
-  color: #fff;
-  background-color: #354052;
-}
-.content {
-  width: 100%;
-  min-height: 150px;
-  overflow: auto;
-}
 </style>
